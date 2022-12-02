2c91f018da44a6a30e00a59833c74d9bb9781bf5
Kato Steven Mubiru <kato.steven60@gmail.com>
2022-12-02 12:12:44 +0300

    Add files via upload

---
+++ Views/RipeningStages.swift
@@ -0,0 +1,20 @@
+//
+//  RipeningStages.swift
+//  Kedo
+//
+//  Created by Kato Steven Mubiru on 30/11/2022.
+//
+
+import SwiftUI
+
+struct RipeningStages: View {
+    var body: some View {
+        Text("Ripening Stages")
+    }
+}
+
+struct RipeningStages_Previews: PreviewProvider {
+    static var previews: some View {
+        RipeningStages()
+    }
+}
---
+++ Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  Kedo
+//
+//  Created by Kato Steven Mubiru on 30/11/2022.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
